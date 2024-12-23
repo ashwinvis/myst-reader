2400cf99d1457779165b341971a9377d43a1253a
Ashwin V. Mohanan <dev@fluid.quest>
2024-12-23 03:22:09 +0100

    Use uv in nox
    https://nox.thea.codes/en/stable/cookbook.html

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,10 @@ import nox
 
 PACKAGE = "pelican-myst-reader"
 CWD = Path.cwd()
-if (CWD / "poetry.lock").exists():
+if (CWD / "uv.lock").exists():
+    BUILD_SYSTEM = "uv"
+    PACKAGE_SPEC = "pyproject.toml"
+elif (CWD / "poetry.lock").exists():
     BUILD_SYSTEM = "poetry"
     PACKAGE_SPEC = "pyproject.toml"
 else:
@@ -149,7 +152,17 @@ def pip_compile(session, extra):
 
 
 def install_with_tests(session, args=()):
-    if BUILD_SYSTEM == "poetry":
+    if BUILD_SYSTEM == "uv":
+        session.run_install(
+            "uv",
+            "sync",
+            "--group",
+            "tests",
+            *args,
+            env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+        )
+        return "python", "-m", "pytest"
+    elif BUILD_SYSTEM == "poetry":
         session.install("poetry")
         session.run("python", "-m", "poetry", "install", "--with=tests", *args)
         session.run("python", "-m", "poetry", "env", "info")
